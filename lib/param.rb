f44297e2849c7ae06d8733bf3e098f06758b0af4
Rake Yang <fenglaijun@gmail.com>
2019-10-29 17:40:18 +0800

    脚本优化拆分、支持切换bundleid（含apple watch)

---
+++ lib/param.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby -w
+require 'fileutils'
+require 'yaml'
+require_relative './util'
+
+Arg_Name_ConfigPath   = "--config"
+ACTION_TYPE_PREFIX  = "prefix"
+ACTION_TYPE_IMAGE   = "image"
+ACTION_TYPE_SWITCH  = "switch"
+
+# author: Rake Yang
+# 参数、配置文件解析
+# 2019-10-29
+class PBParam
+  attr_accessor:directory   # 工作目录
+  attr_accessor:projectPath # 项目文件目录
+  attr_accessor:sourcePaths # 源码目录列表
+  attr_accessor:imagePaths  # 图片目录列表
+  attr_accessor:old_prefix  # 之前前缀
+  attr_accessor:new_prefix  # 新前缀
+  attr_accessor:ignorePaths # 忽略目录列表
+  attr_accessor:prefixMap   # 替换的映射表
+  attr_accessor:sourceFiles # 源码文件列表
+  attr_accessor:includeCategory # 包含类别
+  attr_accessor:podfile     # podfile
+  attr_accessor:actionType  # 命令类型
+  attr_accessor:bundle  #App配置
+  def initialize(args)
+    @ignorePaths = []
+    @sourceFiles = []
+    @prefixMap = Hash.new
+
+    @actionType = args[0]
+    if @actionType.nil? || ![ACTION_TYPE_IMAGE, ACTION_TYPE_SWITCH, ACTION_TYPE_PREFIX].include?(@actionType)
+      showUsage()
+      return
+    end
+    configPath = args[1].eql?(Arg_Name_ConfigPath) ? args[2] : '.toolbox.yml'
+    if configPath && File.exist?(configPath)
+      properties = YAML.load_file(Pathname(configPath).to_path)
+      @directory = properties['directory']
+      Dir.chdir(@directory)
+      @projectPath = properties['project']
+      @sourcePaths = properties['sourcePaths']
+      @ignorePaths = properties['ignorePaths']||[]
+      @imagePaths  = properties["imagePaths"]
+      @includeCategory = properties['includeCategory']
+      @podfile = properties['podfile']
+      @old_prefix = properties['prefix'].split("=").first
+      @new_prefix = properties['prefix'].split("=").last
+      @bundle = properties['bundle']
+    else
+      puts PBUtil::error("缺少参数#{Arg_Name_ConfigPath}且者找不到默认文件.toolbox.yml #{Dir.pwd}",true)
+      PBParam::showUsage()
+    end
+  end
+
+  def showUsage()
+    puts "用法: command #{PBUtil::warn('[选项]')}
+
+命令:
+    image         修改图片hash
+    prefix        修改类目前缀,暂不支持内部类
+    switch        切换App（马甲包）
+
+选项:
+    --config      配置文件目录（推荐, 其它参数将失效）
+    "
+  end
+
+end
---
+++ lib/switch.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/ruby -w
+require 'fileutils'
+require_relative './util'
+require 'json'
+require 'xcodeproj'
+
+# author: Rake Yang
+# 切换App配置
+# 2019-10-29
+class PBSwitch
+  def initialize(param)
+    doAction(param)
+  end
+
+  def doAction(param)
+    index = 0
+    param.bundle['configurations'].each{|key,value|(
+      index = index + 1
+      puts PBUtil.warn("#{index}.#{key['product_name']}")
+      )}
+    toidx = choose(param.bundle['configurations'].count)
+    exchangeTo(param, toidx)
+  end
+
+  def choose(max)
+    puts PBUtil.info("请选择[1-#{max}]:")
+    toidx = Integer(STDIN.gets)
+    if toidx < 1 || toidx > max
+      puts PBUtil.error("输出错误，请重试")
+      choose(max)
+    end
+    return toidx
+  end
+
+  def exchangeTo(param, index)
+    puts PBUtil.debug("替换bundle等App信息")
+    project = Xcodeproj::Project.open(param.projectPath)
+    config = param.bundle['configurations'][index-1]
+    config['schemes'].each{|scheme|(
+      target = project.targets.select{|t| t.name.eql?(scheme['name'])}.first
+      if target.nil?
+        next
+      end
+      puts PBUtil.debug(target.product_type)
+      target.build_configurations.each{|buildc| (
+        buildc.build_settings['DEVELOPMENT_TEAM'] = config['team_id']
+        buildc.build_settings['PROVISIONING_PROFILE_SPECIFIER'] = scheme[buildc.name]
+        info_plist = buildc.build_settings['INFOPLIST_FILE'].gsub(/\$\(SRCROOT\)\//, "")
+          # App显示名称
+          `/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName #{config['product_name']}" #{info_plist}`
+          # BundleID
+          `/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier #{scheme['bundleid']}" #{info_plist}`
+          if target.product_type.eql?('com.apple.product-type.application.watchapp2')
+            # Apple Watch
+            `/usr/libexec/PlistBuddy -c "Set :WKCompanionAppBundleIdentifier #{scheme['companionbundleid']}" #{info_plist}`
+          end
+        )}
+    )}
+    project.save()
+    copyResources(param, config)
+  end
+
+  def copyResources(param, config)
+    puts PBUtil.debug("替换资源文件")
+    param.bundle['resources'].each{|res| (
+        shell = "cp -R bundles/#{config['name']}/#{res.keys.first} #{res.values.first}"
+        # puts shell
+        `#{shell}`
+    )}
+  end
+
+end
