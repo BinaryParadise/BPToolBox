fa9f1c9f000e839c088965a62df6c6cb07ddc277
Rake Yang <fenglaijun@gmail.com>
2021-05-18 14:17:57 +0800

    pod发布支持版本选择

--- lib/pod.rb
+++ lib/pod.rb
@@ -2,10 +2,44 @@ require 'fileutils'
 require_relative './util'
 require 'json'
 require 'pathname'
+require 'dispel'
+require 'versionomy'
 
 VERSION_REGEX = /(.version\s*=\s*["'])(\d+.\d+.\d+)(["'])/i
 
+class PBPodVersionPicker
+    attr_accessor:position
+    attr_accessor:playlist
+    attr_accessor:cur
+    attr_accessor:confirm
+
+    def initialize(ver)
+        @cur = Versionomy.parse(ver)
+
+        @playlist = Array.new
+        @playlist.push("#{@cur.major}.#{@cur.minor}.#{@cur.tiny+1}")
+        @playlist.push("#{@cur.major}.#{@cur.minor+1}.#{0}")
+        @playlist.push("#{@cur.major+1}.#{0}.#{0}")
+        @position = 0
+    end
+
+    def newVer()
+        return @playlist[@position]
+    end
+  
+    def show_playlist
+        vt = ["主要", "次要", "修正"]
+        @playlist.each_with_index.map do |song, index|
+            position == index ? "※ #{vt[index]} #{song}" : "  #{vt[index]} #{song} "
+        end
+    end
+end
+
 class PBPodUtility
+    def show_ui playlist_obj
+        ["\n", playlist_obj.show_playlist, "\n当前版本: #{playlist_obj.cur.to_s} "].join("\n")
+    end
+
     def initialize(param)
         conf = "#{File.dirname(__FILE__)}/../pod.config.json"
         params = param.params
@@ -29,6 +63,11 @@ class PBPodUtility
     def setRepo(conf, repo)
         puts conf
         config = Hash.new
+        if repo.nil?
+            config = JSON.parse(File.read(conf))
+            puts PBUtil.warn("请输入repo名称，当前为#{config['repo']}")
+            return
+        end
         config['repo'] = repo
         json_file = File.new(conf, 'w+')
         json_file.write(JSON.pretty_generate(config))
@@ -50,17 +89,41 @@ class PBPodUtility
             PBUtil.error("[!] Unable to find a podspec in the working directory")
         else
             file_content = File.read(podspec)
+            curver = nil
             file_content.scan(VERSION_REGEX) do|match|
-                puts PBUtil.warn("当前版本#{match[1]}")
+                curver = match[1]
             end
 
-            print "请输入要发布的版本: \n"
-            new_version = STDIN.gets.chomp
-            if new_version.nil? || new_version.scan(/(\d+.\d+.\d+)/).length ==0 
-                puts PBUtil.error("版本号不符合规范，请重新输入!")
+            if curver.nil? || curver.scan(/(\d+.\d+.\d+)/).length ==0 
+                puts PBUtil.error("[!] 未找到符合的版本号 #{curver}")
                 return
             end
-            print "确认发布版本#{new_version}? [yN]\n"
+
+            pd = PBPodVersionPicker.new(curver)
+
+            Dispel::Screen.open do |screen|
+                screen.draw show_ui(pd)
+              
+                Dispel::Keyboard.output do |key|
+                  case key
+                  when :up then pd.position = [pd.position-1, 0].max
+                  when :down then pd.position = [pd.position+1, pd.playlist.length-1].min
+                  when :enter then pd.confirm = true
+                  when "q" then break
+                  end
+                  if pd.confirm 
+                    break
+                  end
+                  screen.draw show_ui(pd)
+                end
+            end
+
+            if !pd.confirm
+                puts "取消发布"
+                return
+            end
+            new_version = pd.newVer()
+            puts PBUtil.warn("确认发布版本#{new_version.to_s}? [yN]\n")
             if STDIN.gets.chomp.downcase.eql?('y')
                 name = podspec.gsub(/.podspec/, '')
                 puts PBUtil.info("开始发布#{name}新版本[#{new_version}]")
@@ -96,6 +159,10 @@ class PBPodUtility
                 puts PBUtil.debug("#{name} #{new_version} 发布成功")
             end
         end
+    end
+
+    def versionSelect()
+        
     end
 
     def showUsage()
